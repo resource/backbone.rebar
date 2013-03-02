0e53ca506ed6c12e9d1fc6a25124c2d5285ea1e9
Eric McGary <emcgary@resource.com>
2013-03-02 10:47:48 -0500

    Adding docs

---
+++ docs/api.js
@@ -0,0 +1,16 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Backbone.Rebar.Application",
+        "Backbone.Rebar.CompositeView",
+        "Backbone.Rebar.Controller",
+        "Backbone.Rebar.DependencyRouter",
+        "Backbone.Rebar.Logger",
+        "Backbone.Rebar.Mediator",
+        "Backbone.Rebar.PersistenceModel",
+        "Backbone.Rebar.View"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
